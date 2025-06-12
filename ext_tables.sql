894111f25a8502ebe8002fd7d6695cce8017d973
Konrad Michalik <km@move-elevator.de>
2025-06-12 14:02:15 +0200

    feat: update default value for technical headline tag to 'h2'

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,4 @@
 CREATE TABLE tt_content
 (
-	tx_metypo3styleguide_technicalheadlinetag varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+	tx_metypo3styleguide_technicalheadlinetag varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'h2',
 );
